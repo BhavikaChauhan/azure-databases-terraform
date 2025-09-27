b38312de64a9ae33ab27651877deaead3c13f7a5
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-09-27 20:27:12 +0530

    Create main.tf

---
+++ modules/cosmos/main.tf
@@ -0,0 +1,30 @@
+resource "azurerm_cosmosdb_account" "cosmos" {
+  name                = "cosmos-${var.resource_group_name}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  consistency_policy {
+    consistency_level = "Session"
+  }
+
+  geo_location {
+    location          = var.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "db" {
+  name                = "appdb"
+  resource_group_name = var.resource_group_name
+  account_name        = azurerm_cosmosdb_account.cosmos.name
+}
+
+resource "azurerm_cosmosdb_sql_container" "container" {
+  name                = "items"
+  resource_group_name = var.resource_group_name
+  account_name        = azurerm_cosmosdb_account.cosmos.name
+  database_name       = azurerm_cosmosdb_sql_database.db.name
+  partition_key_path  = "/id"
+}

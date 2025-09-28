3c8beec24aaf9603944e4a9ac2713326711eadbc
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-09-28 22:25:32 +0530

    Update variables.tf

--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -1,3 +1,14 @@
-variable "resource_group_name" { type = string }
-variable "location" { type = string }
-variable "storage_account_name" { type = string }
+variable "storage_account_name" {
+  description = "Storage account name (lowercase, 3-24 chars, letters+numbers only)"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource group name where the storage account will be created"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}

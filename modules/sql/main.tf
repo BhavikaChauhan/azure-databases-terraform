f0d32472c5bd00280d51be025e1d605d4e67b92b
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-09-28 16:30:02 +0530

    Update main.tf

--- modules/sql/main.tf
+++ modules/sql/main.tf
@@ -3,22 +3,25 @@ resource "random_password" "sql_password" {
   special = true
   upper   = true
   lower   = true
-  number  = true
+  numeric = true
 }
 
 resource "azurerm_mssql_server" "sql_server" {
   name                         = var.sql_server_name
   resource_group_name          = var.resource_group_name
   location                     = var.location
-  administrator_login          = var.sql_admin_username
-  administrator_login_password = var.sql_admin_password != "" ? var.sql_admin_password : random_password.sql_password.result
   version                      = "12.0"
-  minimum_tls_version          = "1.2"
+  administrator_login          = "sqladmin"
+  administrator_login_password = var.sql_admin_password
+
+  public_network_access_enabled = true
+  minimum_tls_version           = "1.2"
 }
 
 resource "azurerm_mssql_database" "sql_db" {
-  name                = var.sql_database_name
-  server_id           = azurerm_mssql_server.sql_server.id
-  resource_group_name = var.resource_group_name
-  sku_name            = "S0"
+  name       = var.sql_database_name
+  server_id  = azurerm_mssql_server.sql_server.id
+  sku_name   = "S0"
+  collation  = "SQL_Latin1_General_CP1_CI_AS"
+  max_size_gb = 5
 }

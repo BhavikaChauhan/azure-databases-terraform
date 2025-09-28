ece426762410afc4560767f2f15a7442911392cf
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-09-28 21:45:11 +0530

    Update outputs.tf

--- modules/sql/outputs.tf
+++ modules/sql/outputs.tf
@@ -1,3 +1,7 @@
-output "sql_database_name" {
-  value = azurerm_mssql_database.sql_db.name
+output "storage_account_name" {
+  value = azurerm_storage_account.storage.name
+}
+
+output "primary_blob_endpoint" {
+  value = azurerm_storage_account.storage.primary_blob_endpoint
 }

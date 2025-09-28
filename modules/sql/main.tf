10e781ef1edf1c059fc0492bcea3377cd26de256
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-09-28 22:20:59 +0530

    Update main.tf

--- modules/sql/main.tf
+++ modules/sql/main.tf
@@ -2,16 +2,13 @@ resource "azurerm_mssql_server" "sql_server" {
   name                         = var.sql_server_name
   resource_group_name          = var.resource_group_name
   location                     = var.location
+  version                      = "12.0"
   administrator_login          = var.sql_admin_username
   administrator_login_password = var.sql_admin_password
-  version                      = "12.0"
-  public_network_access_enabled = true
 }
 
 resource "azurerm_mssql_database" "sql_db" {
-  name                = var.sql_database_name
-  server_id           = azurerm_mssql_server.sql_server.id
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  sku_name            = "S0"
+  name      = var.sql_database_name
+  server_id = azurerm_mssql_server.sql_server.id
+  sku_name  = "S0"
 }

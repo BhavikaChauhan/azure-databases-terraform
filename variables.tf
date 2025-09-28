5379ed7ba71f1bff7fd064e0c58d7aa77a89a924
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-09-28 16:11:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,55 @@
 variable "location" {
-  type    = string
-  default = "eastus"
+  description = "Azure region"
+  type        = string
+  default     = "eastus"
 }
 
 variable "resource_group_name" {
-  type    = string
-  default = "my-database-rg"
+  description = "Root resource group name"
+  type        = string
+  default     = "my-database-rg"
+}
+
+variable "sql_server_name" {
+  description = "SQL Server name (unique within Azure)"
+  type        = string
+}
+
+variable "sql_database_name" {
+  description = "SQL Database name"
+  type        = string
+}
+
+variable "sql_admin_username" {
+  description = "SQL admin username"
+  type        = string
+  default     = "sqladmin"
+}
+
+variable "sql_admin_password" {
+  description = "SQL admin password (sensitive)"
+  type        = string
+  sensitive   = true
+}
+
+variable "cosmos_account_name" {
+  description = "Cosmos DB account name (lowercase, 3-44 chars, letters/numbers/hyphen allowed)"
+  type        = string
+}
+
+variable "cosmos_database_name" {
+  description = "Cosmos DB database name"
+  type        = string
+  default     = "appdb"
+}
+
+variable "cosmos_container_name" {
+  description = "Cosmos DB SQL container name"
+  type        = string
+  default     = "items"
+}
+
+variable "storage_account_name" {
+  description = "Storage account name (lowercase, 3-24 chars, letters+numbers only)"
+  type        = string
 }

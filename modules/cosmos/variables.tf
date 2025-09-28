492afc2e13888d7193051ef252f404fe6814845f
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-09-28 22:23:34 +0530

    Update variables.tf

--- modules/cosmos/variables.tf
+++ modules/cosmos/variables.tf
@@ -1,5 +1,26 @@
-variable "resource_group_name" { type = string }
-variable "location" { type = string }
-variable "cosmos_account_name" { type = string }
-variable "cosmos_database_name" { type = string }
-variable "cosmos_container_name" { type = string }
+variable "cosmos_account_name" {
+  description = "Cosmos DB account name (lowercase, 3-44 chars, letters/numbers/hyphen allowed)"
+  type        = string
+}
+
+variable "cosmos_database_name" {
+  description = "Cosmos DB SQL database name"
+  type        = string
+  default     = "appdb"
+}
+
+variable "cosmos_container_name" {
+  description = "Cosmos DB SQL container name"
+  type        = string
+  default     = "items"
+}
+
+variable "resource_group_name" {
+  description = "Resource group where Cosmos DB will be created"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}

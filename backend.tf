5637f069c9f0a66ba20ecf9b566ff95f1be44943
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-09-28 15:53:46 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,14 @@
- terraform {
- backend "azurerm" {
-   resource_group_name  = "terraform-backend-rg"
-   storage_account_name = "tfbackendstorageabc123"  # replace after bootstrap
-   container_name       = "tfstate"
-    key                  = "infra.tfstate"
- }
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "tfstate-rg"
+    storage_account_name  = "tfstatebhavika123"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
 }
 
 

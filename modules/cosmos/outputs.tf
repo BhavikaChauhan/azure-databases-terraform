1e04bdbdc4892214e8fbac2e06f3d8d8d4b5b06a
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-09-28 22:24:27 +0530

    Update outputs.tf

--- modules/cosmos/outputs.tf
+++ modules/cosmos/outputs.tf
@@ -1,7 +1,19 @@
-output "cosmos_database_name" {
-  value = azurerm_cosmosdb_sql_database.db.name
+output "cosmosdb_account_name" {
+  description = "The name of the Cosmos DB account"
+  value       = azurerm_cosmosdb_account.cosmos.name
 }
 
-output "cosmos_endpoint" {
-  value = azurerm_cosmosdb_account.cosmos.endpoint
+output "cosmosdb_database_name" {
+  description = "The name of the Cosmos DB SQL database"
+  value       = azurerm_cosmosdb_sql_database.db.name
+}
+
+output "cosmosdb_container_name" {
+  description = "The name of the Cosmos DB SQL container"
+  value       = azurerm_cosmosdb_sql_container.container.name
+}
+
+output "cosmosdb_endpoint" {
+  description = "The Cosmos DB account endpoint"
+  value       = azurerm_cosmosdb_account.cosmos.endpoint
 }

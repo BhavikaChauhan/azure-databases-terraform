3254c64c6824c8028097f2d01a777164f163a47a
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-09-28 16:13:48 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,21 @@
 output "sql_server_name" {
-  value = module.sql.sql_server_name
+  value     = module.sql.sql_server_name
+  sensitive = true
 }
 
 output "sql_database_name" {
   value = module.sql.sql_database_name
 }
 
-output "cosmos_endpoint" {
-  value = module.cosmos.endpoint
+output "cosmos_account_name" {
+  value = module.cosmos.cosmos_account_name
 }
 
-output "cosmos_database" {
-  value = module.cosmos.database_name
+output "cosmos_endpoint" {
+  value = module.cosmos.cosmos_endpoint
+  sensitive = true
 }
 
 output "storage_account_name" {
   value = module.storage.storage_account_name
-}
-
-output "blob_container_name" {
-  value = module.storage.container_name
 }

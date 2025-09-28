22a9115322e1690bb89a984b28229fb9f1613e96
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-09-28 16:17:40 +0530

    Update main.tf

--- modules/sql/main.tf
+++ modules/sql/main.tf
@@ -1,19 +1,24 @@
 resource "random_password" "sql_password" {
   length  = 16
   special = true
+  upper   = true
+  lower   = true
+  number  = true
 }
 
 resource "azurerm_mssql_server" "sql_server" {
-  name                         = "sqlserver-${random_password.sql_password.result}"
+  name                         = var.sql_server_name
   resource_group_name          = var.resource_group_name
   location                     = var.location
+  administrator_login          = var.sql_admin_username
+  administrator_login_password = var.sql_admin_password != "" ? var.sql_admin_password : random_password.sql_password.result
   version                      = "12.0"
-  administrator_login          = var.sql_admin_user
-  administrator_login_password = random_password.sql_password.result
+  minimum_tls_version          = "1.2"
 }
 
 resource "azurerm_mssql_database" "sql_db" {
-  name      = "mydb"
-  server_id = azurerm_mssql_server.sql_server.id
-  sku_name  = "S0"
+  name                = var.sql_database_name
+  server_id           = azurerm_mssql_server.sql_server.id
+  resource_group_name = var.resource_group_name
+  sku_name            = "S0"
 }

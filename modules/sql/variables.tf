1649592fd6eb78967b1d52351a3b8b67c56bd350
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-09-28 22:21:27 +0530

    Update variables.tf

--- modules/sql/variables.tf
+++ modules/sql/variables.tf
@@ -1,6 +1,31 @@
-variable "resource_group_name" { type = string }
-variable "location" { type = string }
-variable "sql_server_name" { type = string }
-variable "sql_database_name" { type = string }
-variable "sql_admin_username" { type = string }
-variable "sql_admin_password" { type = string }
+variable "sql_server_name" {
+  description = "SQL Server name (unique within Azure)"
+  type        = string
+}
+
+variable "sql_database_name" {
+  description = "SQL Database name"
+  type        = string
+}
+
+variable "sql_admin_username" {
+  description = "SQL admin username"
+  type        = string
+  default     = "sqladmin"
+}
+
+variable "sql_admin_password" {
+  description = "SQL admin password (sensitive)"
+  type        = string
+  sensitive   = true
+}
+
+variable "resource_group_name" {
+  description = "Resource group where SQL resources will be created"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}

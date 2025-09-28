4aa0b397d19864f861deeaa4347739f0c49b51de
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-09-28 22:14:05 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "cosmosdb_account_name" {
   value = module.cosmos.cosmosdb_account_name
 }
 
+output "cosmosdb_endpoint" {
+  value = module.cosmos.cosmosdb_endpoint
+}
+
 output "sql_server_name" {
   value = module.sql.sql_server_name
 }

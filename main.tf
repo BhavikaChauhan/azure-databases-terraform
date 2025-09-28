5280f16a17cc4957a562960dac82160c0113bc3e
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-09-28 16:13:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,30 @@
-# Create Resource Group for databases
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-# Call SQL Module
-module "sql" {
-  source              = "./modules/sql"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  sql_admin_user      = "sqladmin"
-}
-
-# Call Cosmos Module
-module "cosmos" {
-  source              = "./modules/cosmos"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-}
-
-# Call Storage Module
 module "storage" {
-  source              = "./modules/storage"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
+  source               = "./modules/storage"
+  resource_group_name  = azurerm_resource_group.rg.name
+  location             = var.location
+  storage_account_name = var.storage_account_name
+}
+
+module "cosmos" {
+  source               = "./modules/cosmos"
+  resource_group_name  = azurerm_resource_group.rg.name
+  location             = var.location
+  cosmos_account_name  = var.cosmos_account_name
+  cosmos_database_name = var.cosmos_database_name
+  cosmos_container_name = var.cosmos_container_name
+}
+
+module "sql" {
+  source               = "./modules/sql"
+  resource_group_name  = azurerm_resource_group.rg.name
+  location             = var.location
+  sql_server_name      = var.sql_server_name
+  sql_database_name    = var.sql_database_name
+  sql_admin_username   = var.sql_admin_username
+  sql_admin_password   = var.sql_admin_password
 }

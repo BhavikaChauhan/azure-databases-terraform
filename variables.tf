ac6d108b586816ff3c208010a690fd727c6af616
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-09-28 22:12:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,55 +1,25 @@
-variable "location" {
-  description = "Azure region"
-  type        = string
-  default     = "eastus"
-}
-
 variable "resource_group_name" {
-  description = "Root resource group name"
+  description = "Name of the resource group"
   type        = string
-  default     = "my-database-rg"
+  default     = "bhavika-databases-rg"
 }
 
-variable "sql_server_name" {
-  description = "SQL Server name (unique within Azure)"
+variable "location" {
+  description = "Azure location for resources"
   type        = string
+  default     = "East US"
 }
 
-variable "sql_database_name" {
-  description = "SQL Database name"
-  type        = string
-}
+variable "cosmos_account_name" { type = string }
+variable "cosmos_database_name" { type = string }
+variable "cosmos_container_name" { type = string }
 
-variable "sql_admin_username" {
-  description = "SQL admin username"
-  type        = string
-  default     = "sqladmin"
-}
-
+variable "sql_server_name" { type = string }
+variable "sql_database_name" { type = string }
+variable "sql_admin_username" { type = string }
 variable "sql_admin_password" {
-  description = "SQL admin password (use environment variable)"
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
-variable "cosmos_account_name" {
-  description = "Cosmos DB account name (lowercase, 3-44 chars, letters/numbers/hyphen allowed)"
-  type        = string
-}
-
-variable "cosmos_database_name" {
-  description = "Cosmos DB database name"
-  type        = string
-  default     = "appdb"
-}
-
-variable "cosmos_container_name" {
-  description = "Cosmos DB SQL container name"
-  type        = string
-  default     = "items"
-}
-
-variable "storage_account_name" {
-  description = "Storage account name (lowercase, 3-24 chars, letters+numbers only)"
-  type        = string
-}
+variable "storage_account_name" { type = string }

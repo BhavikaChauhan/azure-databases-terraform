04ac66c2e5ae4455aeebed1b252ca4e80b7bf18f
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-09-28 19:25:56 +0530

    Update

--- main.tf
+++ main.tf
@@ -1,29 +1,3 @@
-terraform {
-  required_version = ">= 1.0.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
-
-  backend "azurerm" {
-    resource_group_name   = "tfstate-rg"
-    storage_account_name  = "tfstatebhavika123"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location

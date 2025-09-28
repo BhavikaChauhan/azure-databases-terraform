6b4251098a391e6c9fd23fa6d226c0f58f07ff5c
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-09-28 16:26:32 +0530

    Update variables.tf

--- modules/sql/variables.tf
+++ modules/sql/variables.tf
@@ -1,6 +1,9 @@
 variable "sql_server_name" { type = string }
 variable "sql_database_name" { type = string }
 variable "sql_admin_username" { type = string }
-variable "sql_admin_password" { type = string, sensitive = true }
+variable "sql_admin_password" {
+  type      = string
+  sensitive = true
+}
 variable "resource_group_name" { type = string }
 variable "location" { type = string }

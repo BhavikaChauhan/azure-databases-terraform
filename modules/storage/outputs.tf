d6df6d8e4fb77edd70b0fe2c056f3105de02cdf5
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-09-28 21:47:41 +0530

    Update outputs.tf

--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -1,7 +1,7 @@
 output "storage_account_name" {
-  value = azurerm_storage_account.this.name
+  value = azurerm_storage_account.storage.name
 }
 
 output "primary_blob_endpoint" {
-  value = azurerm_storage_account.this.primary_blob_endpoint
+  value = azurerm_storage_account.storage.primary_blob_endpoint
 }

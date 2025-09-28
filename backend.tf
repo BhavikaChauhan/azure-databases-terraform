ccd33a416245452615e460cd7ad1cc0e96033f77
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-09-28 19:11:01 +0530

    Update

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "tf-backend-rg"
-    storage_account_name  = "tfbackendstorageabc123"
+    resource_group_name   = "tfstate-rg"
+    storage_account_name  = "tfstatebhavika123"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
   }
